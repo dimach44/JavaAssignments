28855f0b4a5cb2359d8933f6c278f27b0a92077a
dimach <dimach44@gmail.com>
2019-09-07 18:22:33 -0400

    Question 2

--- src/com/revature/Q2/Fibonacci.java
+++ src/com/revature/Q2/Fibonacci.java
@@ -1,10 +1,22 @@
 package com.revature.Q2;
 
+import java.util.ArrayList;
+
 public class Fibonacci {
 	
 	public static void main(String[] args) {
-		
+		doFib(25);
 	}
 	
+	static void doFib(int n) {
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		arr.add(1);
+		int temp = 1;
+		for (int i = 0;i < n-1;i++) {
+			arr.add(temp);
+			temp += arr.get(i);
+		}
+		System.out.println(arr);
+	}
 	
 }
